2919871b58db813207871560d8e6a5fdb81b7240
Alessandro Oliveira <alessandro.fillus@gmail.com>
2017-02-22 21:31:40 -0300

    Adicionando EJS no projeto

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(request, response){
     response.send("<html>Portal de Noticias</html>");
 });
 
 app.get("/tecnologia", function(request, response){
-    response.send("<html>Noticias de tecnologia</html>");
+    response.render("secao/tecnologia");
 });
 
 app.listen(3000, function(){
