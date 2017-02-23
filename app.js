279369ea00bc545f931e78b14c60928d8b40763d
Alessandro Oliveira <alessandro.fillus@gmail.com>
2017-02-22 23:10:26 -0300

    Adicionando Primeiro exemplo de modulo common.js

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
+var msg = require('./mod_teste');
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(request, response){
-    response.send("<html>Portal de Noticias</html>");
+    response.render('home/index');
 });
 
-app.get("/tecnologia", function(request, response){
-    response.render("secao/tecnologia");
+app.get("/formulario_inclusao_noticia", function(request, response){
+    response.render("admin/form_add_noticia");
+});
+
+app.get("/noticias", function(request, response){
+    response.render("noticias/noticias");
 });
 
 app.listen(3000, function(){
-    console.log("SERVIDOR RODANDO COM EXPRESS");
-});
+    console.log(msg());
+}); 
