d2195ac83e92d675f22edba83427302c2aa9ead6
Alessandro Oliveira <alessandro.fillus@gmail.com>
2017-03-13 21:06:59 -0300

    Adicionando o Consign no projeto

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias')(app);
+/*var rotaNoticias = require('./app/routes/noticias')(app);
 var rotaHome = require('./app/routes/home');
 rotaHome(app);
 
 var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia');
 rotaFormInclusaoNoticia(app);
-
+*/
 app.listen(3000, function(){
     console.log("SERVIDOR RODANDO COM EXPRESS");
 }); 
--- config/server.js
+++ config/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
