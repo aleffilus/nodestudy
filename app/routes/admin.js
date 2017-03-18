244338fdd65f8cd4cc3f5937e88300f25f72d0b3
Alessandro Oliveira <alessandro.fillus@gmail.com>
2017-03-18 16:15:37 -0300

    Adicionando Express Validator e validando formulário

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,16 +1,32 @@
 module.exports = function(application) {
    application.get("/formulario_inclusao_noticia", function(request, response){
-        response.render("admin/form_add_noticia");
+        response.render("admin/form_add_noticia", {validacoes: {}, noticia:{}});
    });
 
    application.post("/noticias/salvar", function(request, response){
         var noticia = request.body;
 
-        var conexao = application.config.db_connection();
-        var noticiasDAO = new application.app.models.NoticiasDAO(conexao); 
+        request.assert('titulo','Título é obrigatório').notEmpty();
+        request.assert('resumo','Resumo é obrigatório').notEmpty();
+        request.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        request.assert('autor','Autor é obrigatório').notEmpty();
+//        request.assert('data_noticia','Data da Notícia é obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        request.assert('data_noticia','Data da Notícia é obrigatório').notEmpty();
+        request.assert('data_noticia','Data da Notícia é deve ser uma data').isDate({format: 'YYYY-MM-DD'});
+        request.assert('noticia','Notícia é obrigatório').notEmpty();
 
-        noticiasDAO.salvarNoticia(noticia, function(error, result){
-            response.redirect("/noticias");
-        });  
+        var erros = request.validationErrors();
+
+        if(erros == false){
+            var conexao = application.config.db_connection();
+            var noticiasDAO = new application.app.models.NoticiasDAO(conexao); 
+
+            noticiasDAO.salvarNoticia(noticia, function(error, result){
+                response.redirect("/noticias");
+            });  
+        }else{
+            response.render("admin/form_add_noticia", {validacoes: erros, noticia: noticia});
+            return;
+        }
     });
 }; 
